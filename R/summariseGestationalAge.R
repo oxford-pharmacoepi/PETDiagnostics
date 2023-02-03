e776970fb4c6f5849a11ffaf7e8f2836664b8ee8
edward-burn <9583964+edward-burn@users.noreply.github.com>
2023-02-03 09:26:11 +0000

    summGestAge - remove missing argument from cdmconnector dates

--- R/summariseGestationalAge.R
+++ R/summariseGestationalAge.R
@@ -29,10 +29,10 @@ summariseGestationalAge <- function(
       n = dplyr::if_else((!is.na(.data$gestational_length_in_day) &
                            !is.na(.data$pregnancy_start_date) &
                            !is.na(.data$pregnancy_end_date)), dplyr::if_else(dplyr::between(!!CDMConnector::datediff("pregnancy_start_date", "pregnancy_end_date", interval = "day"),
-                                                                                     .data$gestational_length_in_day -7, .data$gestational_length_in_day +7),0, 1,missing = NULL),
+                                                                                     .data$gestational_length_in_day -7, .data$gestational_length_in_day +7),0, 1),
       NA,missing=NULL),
-      endBeforeStart = dplyr::if_else(((!!CDMConnector::dateadd("pregnancy_start_date", minGestAge_Days, interval = "day"))>=.data$pregnancy_end_date),1,0,missing = NULL),
-      endAfterStart = dplyr::if_else(((!!CDMConnector::dateadd("pregnancy_start_date", minGestAge_Days, interval = "day"))<.data$pregnancy_end_date),1,0,missing = NULL)) %>%
+      endBeforeStart = dplyr::if_else(((!!CDMConnector::dateadd("pregnancy_start_date", minGestAge_Days, interval = "day"))>=.data$pregnancy_end_date),1,0),
+      endAfterStart = dplyr::if_else(((!!CDMConnector::dateadd("pregnancy_start_date", minGestAge_Days, interval = "day"))<.data$pregnancy_end_date),1,0)) %>%
         dplyr::collect()
 
 
